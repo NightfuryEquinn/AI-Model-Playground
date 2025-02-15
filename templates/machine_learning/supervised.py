60754ebe3738f9b61c36a428ae3ff625bb01c9a5
NightfuryEquinn <xianzyip8@gmail.com>
2025-02-15 22:35:59 +0800

    Changed to iris data

--- templates/machine_learning/supervised.py
+++ templates/machine_learning/supervised.py
@@ -1,27 +1,34 @@
 import pandas as pd
-from sklearn import svm
+from sklearn import svm, datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
+from matplotlib import pyplot
+import seaborn as sea
+from pandas.plotting import scatter_matrix
 
-# Load dataset
-wine_df = pd.read_csv('wine_data.csv')
-wine_df = wine_df.dropna()
+# Load datasets
+iris = pd.read_csv('iris_data.csv')
 
-# Using support vector machines (SVM)
+# Features
+x = iris.iloc[:, :4]
+# Targets
+s = iris['species']
+
+print(iris.describe())
+
+#######################################
+# Using support vector machines (SVM) #
+#######################################
 def support_vector_machines():
-  # Convert country to unique integer
+  # Convert to unique integer
   le = LabelEncoder()
-  wine_df['variety'] = le.fit_transform(wine_df['variety'])
-
-  # Features
-  x = wine_df.iloc[:, 4:6]
-  # Targets
-  s = wine_df['variety']
+  iris['species'] = le.fit_transform(iris['species'])
 
   # Get unique sorted values
   unique_s = sorted(set(s))
 
   # Create a mapping dictionary
-  # Example: { 'Pinot Noir': 0, 'Syrah': 1, ... }
   mapping_d = {}
   for index, value in enumerate(unique_s):
     mapping_d[value] = index
@@ -33,13 +40,70 @@ def support_vector_machines():
     y.append(numeric_value)
 
   clf = svm.SVC()
-  clf.fit(x, y)
+  clf.fit(x.values, y)
 
-  # Prediction of wine grapes variety based on points and price
-  p = clf.predict([[10.0, 55.0]])
+  # Prediction
+  p = clf.predict([[5.5, 1.2, 4.8, 1.4]])
   p_variety = le.inverse_transform([p[0]])[0]
 
-  print(f"Number of Unique Variety: { len(wine_df['variety'].unique()) }")
-  print(f"Prediction Variety: { p[0] } - { p_variety }")
+  print(f"Number of Unique Species: {len(iris['species'].unique())}")
+  print(f"Prediction Species: {p[0]} - {p_variety}")
+
+  # Display scatterplot
+  def scatterplot():
+    scatter_matrix(iris)
+    pyplot.show()
+
+    iris.hist()
+    pyplot.show()
+
+  scatterplot()
+
+  # Determine accuracy score
+  def determine_score():
+    # Split data to train and test
+    x_train, x_test, y_train, y_test = train_test_split(x, s, test_size = 0.2, random_state = 30)
+
+    # Linear Kernel
+    clf = svm.SVC(kernel = 'linear')
+    clf.fit(x_train, y_train)
+    y_pred = clf.predict(x_test)
+    score = accuracy_score(y_test, y_pred)
+    percentage_score = score * 100
+
+    print(f"Linear Accuracy Score: {percentage_score}%")
+
+    # RBF Kernel
+    clf = svm.SVC(kernel = 'rbf')
+    clf.fit(x_train, y_train)
+    y_pred = clf.predict(x_test)
+    score = accuracy_score(y_test, y_pred)
+    percentage_score = score * 100
+
+    print(f"RBF Accuracy Score: {percentage_score}%")
+
+    # Poly Kernel
+    clf = svm.SVC(kernel = 'poly')
+    clf.fit(x_train, y_train)
+    y_pred = clf.predict(x_test)
+    score = accuracy_score(y_test, y_pred)
+    percentage_score = score * 100
+
+    print(f"Poly Accuracy Score: {percentage_score}%")
+
+    # Sigmoid Kernel
+    clf = svm.SVC(kernel = 'sigmoid')
+    clf.fit(x_train, y_train)
+    y_pred = clf.predict(x_test)
+    score = accuracy_score(y_test, y_pred)
+    percentage_score = score * 100
+
+    print(f"Sigmoid Accuracy Score: {percentage_score}%")
+
+  determine_score()
 
 support_vector_machines()
+
+#####################
+# Using Naive Bayes #
+#####################
